fd260089e1a809243c6d66ad97e1eb7c2c10ba76
phyninja <rabh0153@colorado.edu>
2018-02-14 12:25:07 -0700

    updated Heddarum.sol

--- heddarum.sol
+++ heddarum.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.0;
 contract heddarum{
     address public creator;
     mapping (address => uint256) balances;
+    uint public PRICE = 3000000000000000000; //3 ether
     
     function heddarum(){
         creator = msg.sender;
@@ -17,11 +18,10 @@ contract heddarum{
         return balances[_whoever];
     }
     
-    function create(address _receiver, uint amount){
-        if (msg.sender != creator) return;
-        require (_receiver != 0x0);
-        balances[_receiver] += amount;
-        created(_receiver, amount);
+    function create() payable {
+        require (msg.value >0 && msg.value % PRICE == 0 );
+        balances[msg.sender] += msg.value / PRICE;
+        created(msg.sender, msg.value / PRICE);
     }
     
     function tranfer(address _receiver, uint amount){
